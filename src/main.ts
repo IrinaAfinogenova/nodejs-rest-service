a71b084a48cd0e137c24a51001af36eab8991f71
IrinaAfinogenova <afinogenovair1996@gmail.com>
2022-07-30 15:51:34 +0200

    remove usless comments

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
-// throw new Error('oops')
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
